e0afea0ed449a82a13cd39c44ade936db62b72e9
Bratislav <uveknobelovac@yahoo.com>
2020-02-07 12:11:28 +0100

    Change password in UserTableSeeder.php to csrf version

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -11,7 +11,7 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        $users=array("name"=>"Braca","email"=>"uveknobelovac@yahoo.com","password"=>"Braca29x");
+        $users=array("name"=>"Braca","email"=>"uveknobelovac@yahoo.com","password"=>"$2y$10$1J3YqbBBTQWJKxCVNdDmquASG46wMPiB8pDMX1O5n/1XwsST38tWG");
 
         DB::table('users')->insert( $users );
 
